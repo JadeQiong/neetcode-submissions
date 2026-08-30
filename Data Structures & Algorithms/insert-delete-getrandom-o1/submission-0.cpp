f7f8c04604ddaae7b6ec8c7e749ece3ba841e4e5
JieqiongLiu <47715276+JadeQiong@users.noreply.github.com>
2026-08-30 10:25:07 -0700

    Add: insert-delete-getrandom-o1 - submission-0

---
+++ Data Structures & Algorithms/insert-delete-getrandom-o1/submission-0.cpp
@@ -0,0 +1,38 @@
+class RandomizedSet {
+public:
+    unordered_map<int,int> mp;
+    vector<int> nums;
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        if(mp.count(val)) return false;
+        mp[val] = nums.size();
+        nums.push_back(val);
+        return true;
+    }
+    
+    bool remove(int val) {
+        if(!mp.count(val)) return false;
+        int idx = mp[val];
+        int last = nums.back();
+        nums[idx] = last;
+        mp[last]=idx;
+        nums.pop_back();
+        mp.erase(val);
+        return true;
+    }
+    
+    int getRandom() {
+        return nums[rand()%nums.size()];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
